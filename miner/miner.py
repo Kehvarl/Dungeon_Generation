20d9f715783571e03132c25e9e95656d883ed096
Code Dragon <Kehvarl@gmail.com>
2017-11-25 19:10:24 -0700

    Rewrite Miner

--- miner/miner.py
+++ miner/miner.py
@@ -1,12 +1,41 @@
 from enum import Enum
 from random import randrange, randint
 
+from game_map.game_map import GameMap
+from game_map.rect import Rect
+
 
 class Direction(Enum):
     UP = 0
     RIGHT = 1
     DOWN = 2
     LEFT = 3
+
+    @staticmethod
+    def random_direction():
+        """
+        Pick a random direction
+        :return Direction:
+        """
+        return Direction(randrange(0, 3))
+
+    @staticmethod
+    def next_direction(direction):
+        """
+        Clockwise from the given direction
+        :param int direction:
+        :return Direction:
+        """
+        return Direction((direction + 1) % 4)
+
+    @staticmethod
+    def previos_direction(direction):
+        """
+        Counter-clockwise from the given direction
+        :param int direction:
+        :return Direction:
+        """
+        return Direction((direction - 1) % 4)
 
 
 class Miner:
@@ -23,30 +52,47 @@ class Miner:
         Go back to step 3, until the dungeon is complete
     """
 
-    def __init__(self, width=80, height=25):
-        self.width = width
-        self.height = height
-        self.center = (width // 2, height // 2)
-        self.grid = {}
+    def __init__(self, game_map):
+        """
+        :param GameMap game_map:
+        """
+        self.game_map = game_map
+        self.game_map.clear_map()
+        self.width = game_map.width
+        self.height = game_map.height
         self.features = []
-        self._init_grid()
-        x, y = self.center
-        x, y = self._add_rect(x - 2, y - 2, 5, 5)
-        self.features.append((x, y))
+        self.initial_feature()
 
-    def _init_grid(self):
-        """Set up an empty map grid"""
-        for row in range(0, self.height):
-            for col in range(0, self.width):
-                self.grid[(col, row)] = "#"
+    def initial_feature(self, width=None, height=None):
+        if width is None:
+            width = randint(3, 11)
+        if height is None:
+            height = randint(3, 11)
+        x = self.width//2 + randrange(-width//2, width//2)
+        y = self.width // 2 + randrange(-height//2, height//2)
+        room = Rect(x, y, width, height)
+        self.features.append(room)
+        self._create_room(room)
 
-    @staticmethod
-    def _random_direction():
+    def add_feature(self, x, y, width=None, height=None):
         """
-        Pick a random direction
-        :return Direction:
+        Add a feature to the map
+        :param int x:
+        :param int y:
+        :param int width:
+        :param int height:
+        :return bool: True if the feature was able to be added
         """
-        return Direction(randrange(0, 3))
+        direction = Direction.random_direction()
+        if width is None:
+            width = randint(3, 11)
+        if height is None:
+            height = randint(3, 11)
+
+        x, y = self._get_wall(direction, x, y)
+        if self._scan_direction(direction, x, y, width, height):
+            return True
+        return False
 
     def _get_wall(self, direction, x, y):
         """
@@ -57,7 +103,7 @@ class Miner:
         :return int, int: position of wall
         """
         tx, ty = x, y
-        while 0 < tx < self.width - 1 and 0 < ty < self.height - 1 and self.grid[(tx, ty)] != "#":
+        while 0 < tx < self.width - 1 and 0 < ty < self.height - 1 and not self.game_map.tiles[x][y].block_sight:
             if direction == Direction.UP:
                 ty -= 1
             elif direction == Direction.RIGHT:
@@ -110,55 +156,25 @@ class Miner:
         for row in range(y, y + height + 1):
             for col in range(x, x + width + 1):
                 if 0 < col < self.width-1 and 0 < row < self.height-1:
-                    if self.grid[(col, row)] != "#":
+                    if self.game_map.tiles[col][row].block_sight:
                         return False
                 else:
                     return False
         return True
 
-    def _add_rect(self, x, y, width, height):
+    def _create_room(self, room):
         """
-        Clear a rectangle in the grid
-        :param int x: Starting X
-        :param int y: Starting Y
-        :param int width:
-        :param int height:
-        return int, int: Center of feature
+        Set the tiles of a room to be passable
+        :param Rect room: The room in the map
         """
-        for row in range(y, y + height + 1):
-            for col in range(x, x + width + 1):
-                self.grid[(col, row)] = " "
-        return x + width // 2, y + height // 2
-
-    def add_feature(self):
-        """
-        Add a feature to the map
-        return bool: True if feature was added
-        """
-        x, y = self.features[-1]
-        direction = self._random_direction()
-        width = randint(1, 5)
-        height = randint(1, 5)
-        x, y = self._get_wall(direction, x, y)
-        if self._scan_direction(direction, x, y, width, height):
-            x, y = self._add_rect(x, y, width, height)
-            self.features.append((x, y))
-            return True
-        return False
-
-    def show_grid(self):
-        """
-        Display the current map
-        """
-        for row in range(0, self.height):
-            line = ""
-            for col in range(0, self.width):
-                line += str(self.grid[(col, row)])
-            print(line)
+        # Make interior tiles passable
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.game_map.tiles[x][y].block()
 
 
 if __name__ == "__main__":
-    miner = Miner()
-    while miner.add_feature():
-        pass
-    miner.show_grid()
+    test_map = GameMap(80, 25)
+    miner = Miner(test_map)
+
+    print(miner.game_map.printable_map())
